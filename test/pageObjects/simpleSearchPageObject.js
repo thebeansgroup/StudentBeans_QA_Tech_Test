03442d43a14c5fd6dab9b51132145ca36afb1a57
Stephen <stephenrafferty3@gmail.com>
2021-10-19 00:24:32 +0100

    Test scenarios added, bump to chromedriver version

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,13 +1,40 @@
 const ParentPageObject = require('./parentPageObject')
 
+
 class simpleSearchPageObject extends ParentPageObject {
-  goToHomePage () {
+
+
+  searchBox() {
+    return $("._tf5swf");
+  }
+
+  modalSearchBox() {
+    return $("._1g5dvk1");
+  }
+
+  firstResult() {
+    return $('._63p46ei');
+  }
+
+  goToHomePage() {
     // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
     browser.url('')
   }
 
-  verifyHomePage () {
+  verifyHomePage() {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+  }
+
+  openSearchModal() {
+    this.searchBox().click();
+  }
+
+  search(searchString) {
+    this.modalSearchBox().setValue(searchString);
+  }
+
+  checkFirstResult() {
+    this.isElementEqualToExpected(this.firstResult(), 'Samsung')
   }
 }
 
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,9 +1,24 @@
-const { Given } = require('cucumber')
-const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
+const { Given } = require('cucumber');
+const { When } = require('cucumber');
+const { Then } = require('cucumber');
 
-const simpleSearchPageObject = new SimpleSearchPageObject()
+const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject');
 
-Given('I am on the studentbeans homepage', function () {
+const simpleSearchPageObject = new SimpleSearchPageObject();
+
+Given('I am on the studentbeans homepage', function() {
   simpleSearchPageObject.goToHomePage()
   simpleSearchPageObject.verifyHomePage()
-})
+});
+
+Given('I open the search bar', function() {
+  simpleSearchPageObject.openSearchModal()
+});
+
+When('I enter {string}', function(search) {
+  simpleSearchPageObject.search(search)
+});
+
+Then('I should be shown a search listing for {string}', function(search) {
+  simpleSearchPageObject.checkFirstResult()
+});
