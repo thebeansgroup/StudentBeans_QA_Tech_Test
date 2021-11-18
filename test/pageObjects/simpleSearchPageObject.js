92803cf76251ad81fe1b9a80cb28caba2e80bdc3
Jonathan Blakely <jonathanblakely@macbook-pro.home>
2021-11-18 12:19:52 +0000

    changes made to simpleSearchPageObject.js and simpleSearchStepDef.js

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,14 +1,48 @@
 const ParentPageObject = require('./parentPageObject')
 
+
 class simpleSearchPageObject extends ParentPageObject {
-  goToHomePage () {
-    // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
-    browser.url('')
+
+
+  searchBox() {
+    return $("._tf5swf");
   }
 
-  verifyHomePage () {
-    this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+  modalSearchBox() {
+    return $("._1g5dvk1");
   }
+
+  firstResult() {
+    return $('._63p46ei');
+  }
+
+  searchString() {
+    return $('Samsung');
+  }
+
+  goToHomePage() {
+    // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
+    browser.url('');
+  }
+
+  verifyHomePage() {
+    //this.isElementFoundByText($('h2=Recommended For You'), 'Recommended For You')
+    this.isElementEqualToExpected($('h2=Featured Discounts'), 'Featured Discounts')
+  }
+
+  openSearchModal() {
+    this.searchBox().click();
+  }
+
+  search(searchString) {
+    this.modalSearchBox().setValue(searchString);
+  }
+
+  checkFirstResult() {
+    this.isElementEqualToExpected(this.firstResult(), 'Samsung')
+  }
+
+
 }
 
 module.exports = simpleSearchPageObject
