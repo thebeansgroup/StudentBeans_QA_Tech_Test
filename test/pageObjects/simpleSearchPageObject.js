8b55cd0b9874d8e4489b40780e9573dd6d50e03b
Samir Jamil <samirjamil@bulb.co.uk>
2021-12-01 06:00:32 +0000

    first commit

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -9,6 +9,28 @@ class simpleSearchPageObject extends ParentPageObject {
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
   }
+
+  verifySearchBarOpening () {
+    //clicks accept cookies button
+      const cookies = $('#onetrust-accept-btn-handler').click()
+    //finds and clicks the search bar
+    const searchBar = $('[name="query"]').click()
+
+  }
+
+  enterSearchBarText (string) {
+    //enters a value into the search modal
+      const input = $("[class=_1g5dvk1]").setValue(string)
+    
+  }
+
+  verifySearchResults (string) {
+    //asserts results relevant to search query are shown, reusing ParentPageObject.js
+    this.isElementEqualToExpected($('[class=_63p46ei]'), string)
+    const brand = $('[class=_63p46ei]')
+    brand.click()
+
+  }
 }
 
 module.exports = simpleSearchPageObject
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,9 +1,22 @@
-const { Given } = require('cucumber')
+const { Given, When, Then } = require('cucumber')
 const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
 const simpleSearchPageObject = new SimpleSearchPageObject()
 
-Given('I am on the studentbeans homepage', function () {
+Given('I am on the studentbeans homepage', () =>{
   simpleSearchPageObject.goToHomePage()
   simpleSearchPageObject.verifyHomePage()
+})
+
+Given ('I open the search bar', () =>{
+  simpleSearchPageObject.verifySearchBarOpening()
+
+})
+
+When ('I enter {string}', (string) =>{
+  simpleSearchPageObject.enterSearchBarText(string)
+})
+
+Then ('I should be shown a search listing for {string}', (string)=>{
+  simpleSearchPageObject.verifySearchResults(string)
 })
