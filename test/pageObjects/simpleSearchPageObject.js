c20a8d9ac6b1af268af03ec234bf10de09e65c25
gabe-mac-mini <mangiru.gabriel@gmail.com>
2021-12-04 15:12:40 +0000

    first commit

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,5 +1,6 @@
 const ParentPageObject = require('./parentPageObject')
 
+
 class simpleSearchPageObject extends ParentPageObject {
   goToHomePage () {
     // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
@@ -8,6 +9,26 @@ class simpleSearchPageObject extends ParentPageObject {
 
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+  }
+
+  verifySamsung () {
+    this.isElementEqualToExpected($('._63p46ei=Samsung'), 'Samsung')
+  }
+
+  openSearchBar () {
+    const searchBar = $('._tf5swf')
+    searchBar.click();
+    browser.pause(3000)
+  }
+
+  enterSamsung () {
+    const inputField = $('._1g5dvk1')
+    inputField.setValue('Samsung')
+  }
+
+  acceptCookies() {
+    const accept = $('button=Accept All Cookies')
+    accept.click()
   }
 }
 
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,4 +1,4 @@
-const { Given } = require('cucumber')
+const { Given, When, Then} = require('cucumber')
 const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
 const simpleSearchPageObject = new SimpleSearchPageObject()
@@ -6,4 +6,21 @@ const simpleSearchPageObject = new SimpleSearchPageObject()
 Given('I am on the studentbeans homepage', function () {
   simpleSearchPageObject.goToHomePage()
   simpleSearchPageObject.verifyHomePage()
+  simpleSearchPageObject.acceptCookies()
+  
+})
+
+When('I open the search bar', function() {
+  simpleSearchPageObject.openSearchBar()
+  
+})
+
+When('I enter \"Samsung\"', function () {
+  simpleSearchPageObject.enterSamsung()
+  
+})
+
+Then('I should be shown a search listing for \"Samsung\"', function () {
+  simpleSearchPageObject.verifySamsung()
+  
 })
