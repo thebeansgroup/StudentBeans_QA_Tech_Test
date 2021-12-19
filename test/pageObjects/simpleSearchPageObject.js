a4a9d52c76b08ca4e40abd81c8494a905ea7c32b
Thamarrai-Satish <96311111+Thamarrai-Satish@users.noreply.github.com>
2021-12-19 22:44:16 +0000

    Update simpleSearchPageObject.js

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -8,7 +8,49 @@ class simpleSearchPageObject extends ParentPageObject {
 
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+    console.log(“I am on Student Beans Home page”);
   }
-}
+  
+  acceptCookies() {
+    //Clicks accept cookies button
+    const cookies = await driver.findElement(By.xpath(“//div[@id=‘onetrust-button-group’]/button[2]"));
+    cookies.click();
+  }
+  
+  clickSearchBarStartTyping() {
+    // Click search bar in Home page
+    const searchBar =this.isElementClicked(By.xpath(“//div[@class=‘_1avdemu’]”));
+    searchBar.click();
+  }
+  
+ verifySearchBarStartTyping() {
+    // Verify the search bar
+    var currentURL = driver.getCurrentUrl().toString();
+   
+    console.log("***>>> getCurrentUrl: “+currentURL);
+    this.isElementExist((By.xpath(“//input[@class='_1g5dvk1']"));
+  }
 
-module.exports = simpleSearchPageObject
+ enterSearchBar(string) {
+    //Enter the input value in the search bar
+    const inputValue =(By.xpath(“//input[@class=‘_1g5dvk1’)].setValue(string)
+  }
+
+displaySamsungList(string) {
+    //Verify and display list
+    this.isElementExist($('//*[text()[contains(.,${string})]]'));
+
+    Var element = driver.findElements(By.xpath("//div[@class='_dyvdsb']/span['_63p46ei']"));
+    driver.findElements(By.xpath(“//div[@class='_dyvdsb']")).then(function(elements)
+      {
+       elements.forEach(function (element) 
+        {
+          element.getText().then(function(text)
+          {
+            console.log(text.size();
+            console.log(text);
+          }
+        }
+      }
+    }
+ module.exports = simpleSearchPageObject
