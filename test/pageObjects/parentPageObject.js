e8ad7cd7eab5fc0ee70b62523ecc889f96f558a3
Swati Kharbanda <>
2022-09-24 21:12:20 +0100

    Updated chromedriver version to match chrome browser version & used "When" in feature file instead of "And"

--- test/pageObjects/parentPageObject.js
+++ test/pageObjects/parentPageObject.js
@@ -1,8 +1,8 @@
 const { assert } = require('chai')
 
 class ParentPageObject {
-  async isElementEqualToExpected (element, expectedText) {
-    const rrorMessage = 'Actual does not match expected'
+  async isElementEqualToExpected(element, expectedText) {
+    const errorMessage = 'Actual does not match expected'
     assert(await expect(await element.getText(), errorMessage).to.equal(expectedText))
   }
 }
