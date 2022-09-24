07367791e64dd29bd31a7d93c4e4255f1e78ba46
Swati Kharbanda <>
2022-09-24 21:18:38 +0100

    Added the skeleton step definitions using an extension in VSCode

--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,9 +1,25 @@
-const { Given } = require('@cucumber/cucumber')
+const { Given, When, Then } = require('@cucumber/cucumber')
 const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
 const simpleSearchPageObject = new SimpleSearchPageObject()
 
-Given('I am on the studentbeans homepage', function () {
+Given(/^I am on the studentbeans homepage$/, () => {
   simpleSearchPageObject.goToHomePage()
   simpleSearchPageObject.verifyHomePage()
-})
+});
+
+When(/^I open the search bar$/, () => {
+});
+
+When(/^I enter "([^"]*)"$/, (args1) => {
+});
+
+
+Then(/^I should select the 4th "([^"]*)" search listing$/, (args1) => {
+});
+
+
+
+
+
+
