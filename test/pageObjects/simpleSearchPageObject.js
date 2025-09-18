0e01195fcdeb9fd716ff5a0654b00295be82ce20
Alex Walker <alex.walker@thebeansgroup.com>
2025-09-18 15:30:56 +0100

    Fixing tect test for new round of recruitment

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,13 +1,13 @@
-const ParentPageObject = require('./parentPageObject')
+import ParentPageObject from './parentPageObject'
 
 class SimpleSearchPageObject extends ParentPageObject {
   async goToHomePage () {
     // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
-    await browser.url('')
+    await browser.url('/uk')
   }
 
   async verifyHomePage () {
-    await this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+    await this.isElementEqualToExpected($('h2=Student deals of the day'), 'Student deals of the day')
   }
 }
 
